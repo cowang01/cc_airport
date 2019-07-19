a2181a15875ebc919bae57d13406b1694d294d7f
cowang01 <g_cowan27@hotmail.com>
2019-07-19 16:11:05 +0100

    extensions_v1

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,2 +1,8 @@
 public class Manager {
+
+
+
+    public int weightPerPassenger(Flight flight){
+        return flight.getScheduledPlane().getLoadingWeight() / flight.getScheduledPlane().getCapacity();
+    }
 }
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Flight flightA;
+    Manager manager;
+
+
+
+    @Before
+    public void setup(){
+        flightA = new Flight("113", "BERMUDA", "EDINBURGH", PlaneType.CESSNA172, "17/08/2019  19:00");
+        manager = new Manager();
+    }
+
+
+
+    @Test
+    public void remainingBaggageWeight(){
+        assertEquals(312, manager.weightPerPassenger(flightA));
+    }
+
+    @Test
+    public void weightCurrentlyBooked(){
+        assertEquals(0, manager.bookedBaggageWeight());
+    }
+
+    @Test
+    public void availableWeightOnFlight() {
+        assertEquals(312, manager.totalRemainingWeight());
+    }
 }
